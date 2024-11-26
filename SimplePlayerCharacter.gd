30bdf038ff730bfce9e803fd9d7866b841966cbd
Espen <slitwn@outlook.com>
2024-11-26 21:23:19 +0100

    dogs

--- SimplePlayerCharacter.gd
+++ SimplePlayerCharacter.gd
@@ -1,17 +1,46 @@
-extends Sprite2D
+extends Area2D
 
-var ROTATE_SPEED = 3
-var MOVE_SPEED = 150
+@export var ROTATE_SPEED = 5
+@export var MOVE_SPEED = 1
+var prevPos = []
+
+@export var segmentSprites : Array[Sprite2D]
+@onready var butt : AnimatedSprite2D = $"../Butt"
+
+var frameDelay = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$AnimatedSprite2D.play("default")
+	butt.play("default")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	move_local_x(delta*MOVE_SPEED)
+	if(prevPos.size() > frameDelay * (segmentSprites.size()-1)):
+		var i : int = 0
+		for s in segmentSprites:
+			var newPos = prevPos[i*frameDelay]
+			var nPos : Vector2 = newPos[0]
+			s.position = nPos
+			s.rotation = newPos[1]
+			i=i+1
+		var newPos = prevPos.pop_back()
+		var nPos : Vector2 = newPos[0]
+		butt.position = nPos
+		butt.rotation = newPos[1]
+		
+		
+	
+		#var newPos : Array[Vector2, int] = prevPos.pop_back()
+		#var nPos : Vector2 = newPos[0]
+		#$Sprite2D.global_position = nPos
+		#$Sprite2D.global_rotation = newPos[1]
+	
+	prevPos.push_front([position, rotation])
+	move_local_y(delta*MOVE_SPEED)
 #	position.x += delta*100
 	
 	if(Input.is_key_pressed(KEY_SPACE)):
