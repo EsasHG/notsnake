46223308a39fe577ca9c2fc3a6d4cbbb23a3edbc
Espen <slitwn@outlook.com>
2024-11-27 21:40:21 +0100

    more stuff!!
    - Added dog running back and forth at the bottom of the map
    - added a fountain and bench
    - dog now stops when bumping into something

--- SimplePlayerCharacter.gd
+++ SimplePlayerCharacter.gd
@@ -3,34 +3,63 @@ extends Area2D
 @export var ROTATE_SPEED = 5
 @export var MOVE_SPEED = 1
 var prevPos = []
+var spritesToAdd = 0
+@onready var sprite = preload("res://Assets/BODY_SEGMENT_C.png")
 
-@onready var sprite =  preload("res://Assets/BODY_SEGMENT.png")
-
-@export var segmentSprites : Array[Sprite2D]
+var segmentSprites : Array[Node2D]
 @onready var butt : Sprite2D = $"../Butt"
+var canAddSprites = true
 
 var frameDelay = 1
-
+var segmentsPerSection = 50
+var move = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Head.play("default")
 	$Head/Legs.play("default")
+	$"../Butt/Legs_back".play("default")
 	$"../Butt/Tail".play("default")
-	for i : int in 5:
-		add_sprite()
+	segmentSprites.push_back(butt)
+	for i : int in segmentsPerSection:
+		var s : Sprite2D = Sprite2D.new()
+		s.texture = sprite
+		s.scale.y = 0.25
+		#segmentSprites.push_back(s)
+		segmentSprites.insert(segmentSprites.size()-1,s) #legger til nest sist.
+		add_sibling.call_deferred(s)
 		
 	#butt.play("default")
 	pass # Replace with function body.
 
 func add_sprite():
-	var s : Sprite2D = Sprite2D.new()
-	s.texture = sprite
-	s.scale.y = 0.25
-	add_sibling.call_deferred(s)
-	segmentSprites.push_back(s)
+	if(canAddSprites):
+		spritesToAdd+=segmentsPerSection
+		canAddSprites = false
+		get_tree().create_timer(1.0).timeout.connect(resetSpriteTimer)
+#	var s : Sprite2D = Sprite2D.new()
+#	s.texture = sprite
+#	s.scale.y = 0.25
+#	segmentSprites.push_back(s)
+#	segmentSprites.insert(segmentSprites.size()-1,s) #legger til nest sist.
+#	add_sibling.call_deferred(s)
+
+func resetSpriteTimer():
+	canAddSprites = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	if(!move): return
+	
+	if(spritesToAdd > 0):
+		$Head/Legs.pause()
+		var s : Sprite2D = Sprite2D.new()
+		s.texture = sprite
+		s.scale.y = 0.25
+		segmentSprites.insert(segmentSprites.size()-1,s) #legger til nest sist.
+		add_sibling(s)
+		spritesToAdd-=1
+	$Head/Legs.play()
 	
 	if(prevPos.size() > frameDelay * (segmentSprites.size())):
 		var i : int = 0
@@ -39,12 +68,7 @@ func _process(delta: float) -> void:
 			var nPos1 : Vector2 = newPos1[0]
 			s.position = nPos1
 			s.rotation = newPos1[1]
-			i=i+1
-		var newPos = prevPos.pop_back()
-		var nPos : Vector2 = newPos[0]
-		butt.position = nPos
-		butt.rotation = newPos[1]
-		
+			i=i+1		
 		#var newPos : Array[Vector2, int] = prevPos.pop_back()
 		#var nPos : Vector2 = newPos[0]
 		#$Sprite2D.global_position = nPos
@@ -65,7 +89,9 @@ func _process(delta: float) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if(area.is_in_group("Treats")):
 		print_debug("Adding sprritesa")
-		for i : int in 20:
+		for i : int in segmentsPerSection:
 			add_sprite()
-	
+		area.queue_free()
+	elif(area.is_in_group("Dangers")):
+		move = false
 	pass # Replace with function body.
---
+++ running_dog.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+@export var SPEED = 600
+@export var TARGET_X : int = -829
+var dir : int  = -1
+var start
+var move = true
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	start = position
+
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+	if(!move):
+		return
+		
+	position.x += dir*delta*SPEED
+	
+	if(position.x <= TARGET_X):
+		dir=1
+		move = false
+		get_tree().create_timer(0.5).timeout.connect(flip)
+	elif(position.x >= start.x):
+		get_tree().create_timer(0.5).timeout.connect(flip)
+		move = false
+		dir=-1
+		
+func flip():
+	$Sprite2D.flip_h = !$Sprite2D.flip_h
+	get_tree().create_timer(0.5).timeout.connect(func(): move = true)
+	
