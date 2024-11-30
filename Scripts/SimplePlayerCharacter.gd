ca4e2331f3cb2cb70f15fb1a90e14b1a83f50ed0
Espen <slitwn@outlook.com>
2024-11-30 23:06:17 +0100

    fixed body not dissapearing when winning

--- Scripts/SimplePlayerCharacter.gd
+++ Scripts/SimplePlayerCharacter.gd
@@ -208,6 +208,7 @@ func _on_area_entered(area: Area2D) -> void:
 		var camera = get_tree().root.find_child("Camera2D", true, false)
 		camera.reparent(get_tree().root.find_child("World", true, false))
 		playerControl = false
+		segmentParent.queue_free()
 		queue_free()
 		
 	elif(area.is_in_group("Dangers")):
