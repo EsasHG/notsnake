84fb47594fcbad0d1d4b9098580a31ce4d792465
Espen <espenhg@hotmail.com>
2026-02-07 21:26:19 +0100

    Started on a new map!!!!

---
+++ Scripts/PlayGamesIDs.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name PlayGamesIDs
+static var leaderboards : Dictionary = {
+	"FIELD" = "CgkIso_-xZsLEAIQAw",
+	"PARK" = "CgkIso_-xZsLEAIQAQ",
+	"SQUARE" = "CgkIso_-xZsLEAIQBA",
+	"SMALL" = "CgkIso_-xZsLEAIQBQ",
+	"FOREST" = "CgkIso_-xZsLEAIQBg",
+	"WINTER" = "CgkIso_-xZsLEAIQEQ",
+}
